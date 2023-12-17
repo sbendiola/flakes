35c954849d146fe8c37d3edff9f03a86a8096969
steve bendiola <steve.bendiola@gmail.com>
2023-12-17 07:44:12 -0800

    remove systems

--- flake.nix
+++ flake.nix
@@ -12,10 +12,7 @@
     flake-utils,
   }: let
     supportedSystems = [
-      "aarch64-darwin"
-      "aarch64-linux"
       "x86_64-linux"
-      "x86_64-darwin"
     ];
   in
     flake-utils.lib.eachSystem supportedSystems (
